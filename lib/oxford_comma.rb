b944a8a762f82a70fa54776dc0e3979e9823391c
Alexander Schelchere <ascellie@gmail.com>
2019-05-12 16:27:57 -0400

    First condition met

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+    # this is the string we'll return at the end
+    # If our array just has one element, 
+    str = ""
+    if array.count == 1 
+        # just return that element
+        return str += array[0] 
+    end
 
 end
