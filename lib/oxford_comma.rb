03eed4b3f0558e7347b7efb884b3d5857aec8731
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 04:35:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  return array.first if array.length == 1
 
+  oxford_comma_sent = ""
+  if array.length == 2 
+    oxford_comma_sent = "#{array.first} and #{array[1]}"
+  else
+    array[0...-1].each { |str| oxford_comma_sent << "#{str}, " }
+    oxford_comma_sent << "and #{array.last}"
+  end
+  oxford_comma_sent
 end
